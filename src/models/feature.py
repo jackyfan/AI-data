e20c010e1b5d0fa10bc3405773e5d35fb04489b7
jacky <jackyfan@163.com>
2025-06-07 18:39:18 +0800

    特性

---
+++ src/models/feature.py
@@ -0,0 +1,18 @@
+feature_definitions = {
+    "活跃用户": {
+        "sql": "",
+        "description": "统计当日有过登录行为的vopenid，持续的数据能够反映游戏的受欢迎程度，也是评价游戏品质的重要指标。"
+    },
+    "付费用户": {
+        "sql": "",
+        "description": "每天付费用户"
+    },
+    "付费总额": {
+        "sql": "",
+        "description": "当天用户付费的总额"
+    }
+    #more features
+}
+
+def get_feature_definition(feature_name):
+    pass
