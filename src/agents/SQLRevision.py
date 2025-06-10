1604f682740601d966c9126cf6a238492ccdf9ff
jackyfan <jackyfan@163.com>
2025-06-10 23:24:23 +0800

    x修改SQL

--- src/agents/SQLRevision.py
+++ src/agents/SQLRevision.py
@@ -1,9 +1,24 @@
+import json
 from datetime import datetime
-#修正SQL
-def correct_sql(sql,db_error,history,db,model_name):
+from langchain.schema import SystemMessage
+from langchain_core.messages import HumanMessage, AIMessage, ToolMessage
+from langchain_core.output_parsers import JsonOutputParser
+
+from llm.openai_helper import tool_chain
+from utility.config import Config
+from utility.logger_helper import LoggerHelper
+from utility.unscape_helper import unescape_unicode
+from dal.database import SQLiteDB
+
+config  = Config()
+logger = LoggerHelper.get_logger(__name__)
+db = SQLiteDB('example.sqlite3')
+
+# 修正SQL
+def correct_sql(sql, db_error, history, db, model_name):
     currdate = datetime.now().strftime("%-Y%m-%d")
 
-    system_prompt=f"""
+    system_prompt = f"""
         作为一名专业游戏数据分析师，你非常熟悉游戏行业并且擅长SQL代码开发。你将根据用户提供的SQL代码及数据库返回的错误信息，为用户写出正确的SQL代码。
         -当前日期是{currdate}
         -特别注意，数据库的错误信息和用户信息中的内容可以提升编写SQL代码的成功率。
@@ -20,4 +35,67 @@ def correct_sql(sql,db_error,history,db,model_name):
         严格按照如下JSON格式输出：
         {{"reflect":(对SQL错误较为详细的反思)...,"sql":"..."}}
     """
-    messages = [SystemMessage()]
+    messages = [SystemMessage(system_prompt), *history, HumanMessage(user_prompt)]
+
+
+#修正SQL
+def invoke(message):
+    try:
+        task = json.load(message[-1].content)
+        retry_limit = int(config["LLM"].get("retry", 3))
+        model_name = config["LLM"].get("model_name", "gpt-3.5-turbo")
+    except ValueError as e:
+        logger.error(f'Error parsing retry limit:{e}')
+        retry_limit=3
+    except Exception as e:
+        logger.error(f'Exception during message parsing:{e}')
+        return None
+    logger.info(f'{retry_limit=}\n${task=}\n')
+    eva_sql = task["sql"]
+    history=[]
+    for attempt in range(retry_limit):
+        logger.debug(f'第{attempt}次修正SQL:{eva_sql}')
+        rst = db.explain(eva_sql)
+        logger.info(f'SQL explain:{rst=}')
+        if rst['code']==0:
+            logger.info(f'SQL校验正确')
+            break
+        corrections = correct_sql(eva_sql, rst['message'], history, db, model_name)
+        history.extend(corrections)
+        eva_sql=try_parse_new_sql(history)
+        if eva_sql:
+            task["sql"] = unescape_unicode(eva_sql)
+            message.append(AIMessage(content=eva_sql,role="assistant"))
+    return message
+
+def perform_correction(message,db,model_name):
+    result = []
+    while True:
+        logger.debug("Processing tool_call cycle...")
+        rsp =tool_chain(model_name,message,[getable])
+        message.append(rsp)
+        result.append(rsp)
+        for tool_call in rsp.tool_calls:
+            selected_tool = getable if tool_call["name"].lower() == "getable" else None
+            if selected_tool:
+                tool_output = selected_tool.invoke(tool_call["args"])
+                message.append(ToolMessage(json.dumps(tool_output,ensure_ascii=False),tool_call_id=tool_call["id"],role="tool"))
+                result.append(message[-1])
+        if isinstance(message[-1],ToolMessage):
+            continue
+        else:
+            break
+        return result
+
+#修正历史中提取和返回最新的SQL代码
+def try_parse_new_sql(history):
+    parser = JsonOutputParser()
+    try:
+        last_correction = history[-1].content
+        sql_details = parser.parse(last_correction)
+        return sql_details["sql"] if sql_details["sql"] else ""
+    except Exception as e:
+        logger.error(f'Failed to parsing new SQL:{e}')
+        return ""
+
+
--- src/utility/unscape_helper.py
+++ src/utility/unscape_helper.py
@@ -1,4 +1,6 @@
 from functools import wraps
+import re
+
 
 def access_control(permission_required,get_permission_func):
     def decorator(func):
@@ -11,3 +13,24 @@ def access_control(permission_required,get_permission_func):
                 raise PermissionError(f'Access denied.Required permission:{permission_required}')
         return wrapper
     return decorator
+
+def contains_escaped_unicode(s):
+    """
+    匹配\u后面跟着4个十六进制数字的模式
+    """
+    pattern = re.compile(r'\\u[0-9a-fA-F]{4}')
+    match = pattern.search(s)
+    return match is not None
+def unescape_unicode(s):
+    if contains_escaped_unicode(s):
+        try:
+            new_text =s.replace('\\\\u', '\\u')
+            return replace_escaped_unicode(new_text)
+        except(UnicodeDecodeError,UnicodeEncodeError):
+            return s
+    return s
+def replace_escaped_unicode(s):
+    def replace_match(match):
+        return match.group(0).encode().decode('unicode_escape')
+    pattern = re.compile(r'\\u[0-9a-fA-F]{4}')
+    return pattern.sub(replace_match, s)
